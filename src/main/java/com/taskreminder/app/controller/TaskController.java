2f142fea080aeac799f74e820481ea287a5295d0
Dakarai Mahisa <mahisadakarai@gmail.com>
2025-12-10 12:03:31 +0530

    removing duplicate markAsDone method

--- src/main/java/com/taskreminder/app/controller/TaskController.java
+++ src/main/java/com/taskreminder/app/controller/TaskController.java
@@ -83,20 +83,6 @@ public class TaskController {
         return "redirect:/api/tasks";
     }
 
-
-    //Mark as done method
-    @GetMapping("/tasks/mark-done/{id}")
-    public String markTaskAsDone(@PathVariable Integer id) {
-
-        taskService.findById(id).ifPresent(task -> {
-            task.setStatus("Completed");
-            taskService.updateTask(task);
-        });
-
-        return "redirect:/api/tasks";
-    }
-
-
     //Delete Task
     @GetMapping("/tasks/delete/{id}")
     public String deleteTask(@PathVariable int id){
