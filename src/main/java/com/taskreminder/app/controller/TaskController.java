d55139f2f763ac91ed66aee1d72848dd452b943d
Dakarai Mahisa <mahisadakarai@gmail.com>
2025-12-22 19:06:00 +0530

    adding navbar to all pages

--- src/main/java/com/taskreminder/app/controller/TaskController.java
+++ src/main/java/com/taskreminder/app/controller/TaskController.java
@@ -19,7 +19,7 @@ import java.util.List;
 import java.util.stream.IntStream;
 
 @Controller
-@RequestMapping("/api")
+@RequestMapping(value={"/","","/api"})
 public class TaskController {
 
     @Autowired
